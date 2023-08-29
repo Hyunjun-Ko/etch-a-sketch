32e63d85896512e25b5d2a1be9ba8525190d5355
Hyunjun-Ko <85072422+Hyunjun-Ko@users.noreply.github.com>
2023-08-29 15:28:27 +0900

    add prettier styling

--- app.js
+++ app.js
@@ -3,49 +3,47 @@ const sizeButton = document.querySelector(".size");
 const gridContainer = document.querySelector(".gridContainer");
 
 function resetGrid() {
-    const allGrid = document.querySelectorAll(".grid");
-    for (let i = 0; i < allGrid.length; i++) {
-        allGrid[i].style.backgroundColor = "white";
-    }
+  const allGrid = document.querySelectorAll(".grid");
+  for (let i = 0; i < allGrid.length; i++) {
+    allGrid[i].style.backgroundColor = "white";
+  }
 }
 
 // function that creates grid elements and resizes grid
 function sizeGrid() {
-    let size = prompt(
-        "Enter the size of the grid. (a number between 0 and 100)"
-    );
+  let size = prompt("Enter the size of the grid. (a number between 0 and 100)");
 
-    // size limits
-    if (size >= 100 || size <= 0) {
-        alert("invalid input, defaulting to 16x16 grid");
-        size = 16;
-    }
+  // size limits
+  if (size >= 100 || size <= 0) {
+    alert("invalid input, defaulting to 16x16 grid");
+    size = 16;
+  }
 
-    // remove previous grid if any
-    gridContainer.replaceChildren();
+  // remove previous grid if any
+  gridContainer.replaceChildren();
 
-    // creating grid of selected size
-    for (let i = 0; i < size * size; i++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        grid.style.padding = `${960 / size / 2}px`; // each padding of the square to fit container.
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = "black";
-        });
-        gridContainer.appendChild(grid);
-        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    }
+  // creating grid of selected size
+  for (let i = 0; i < size * size; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.style.padding = `${960 / size / 2}px`; // each padding of the square to fit container.
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = "black";
+    });
+    gridContainer.appendChild(grid);
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  }
 }
 
 // reset button logic
 resetButton.addEventListener("click", () => {
-    resetGrid();
+  resetGrid();
 });
 
 // resize grid button
 sizeButton.addEventListener("click", () => {
-    sizeGrid();
+  sizeGrid();
 });
 
 // initializing grid
